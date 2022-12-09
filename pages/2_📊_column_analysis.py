5e9ac91edfb749af65a85d0ed42fdaa2c3edd7cf
allisonbday <allisonbday@byui.edu>
2022-12-09 12:40:48 -0700

    added pages

---
+++ pages/2_📊_column_analysis.py
@@ -0,0 +1,210 @@
+# IMPORTS ---------------------------------------------------------------------
+import pandas as pd
+import yaml
+import time
+
+# classes
+from fred_resources.fredSubclasses import *
+from functools import reduce
+
+# streamlit
+import streamlit as st
+import plotly.express as px
+
+# YAML ------------------------------------------------------------------------
+yml = r"z_src\setup.yml"
+with open(yml, "r") as file:
+    dataMap = yaml.safe_load(file)
+
+class_list = dataMap["class_list"]
+state_names = dataMap["state_names"]
+full_state_dict = dataMap["full_state_dict"]
+columns = dataMap["columns"]
+
+# FUNCTIONS -------------------------------------------------------------------
+@st.cache
+def read_unintermaster():
+    master = pd.read_csv(r"z_src\final_datasets\uninterpolated_master.csv")
+    master = master.drop(["Unnamed: 0"], axis=1)
+    return master
+
+
+@st.cache(suppress_st_warning=True)
+def pull_all(cls):
+    # # NATIONAL
+    # wait = 10
+    # dfs = []
+    # for cls in class_list:
+    #     func = f"{cls}.pull_all_states_data()"
+    #     df = eval(func)
+    #     dfs.append(df)
+    #     time.sleep(wait)
+    #     st.write(f"✅ {cls} pulled")
+    #     print(f"✅ {cls} pulled")
+
+    # merged_df = reduce(
+    #     lambda left, right: pd.merge(
+    #         left, right, on=["State", "Year", "Quarter", "Month"], how="outer"
+    #     ),
+    #     dfs,
+    # )
+
+    func = f"{cls}.pull_all_states_data()"
+    df = eval(func)
+    return df
+
+
+###############################################################################
+# SET UP ----------------------------------------------------------------------
+st.set_page_config(page_icon="📊", page_title="Data Analysis", layout="wide")
+st.image(
+    "https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/bar-chart_1f4ca.png",
+    width=100,
+)
+st.title("Data Analysis")
+
+# INITIALIZE CLASSES ----------------------------------------------------------
+Bussiness_Applications = BusinessApplications(
+    r"z_src\fred_resources\fred_yamls\BusinessApplications.yml"
+)
+Construction_Employees = ConstructionEmployees(
+    r"z_src\fred_resources\fred_yamls\ConstructionEmployees.yml"
+)
+Construction_Wages = ConstructionWages(
+    r"z_src\fred_resources\fred_yamls\\ConstructionWages.yml"
+)
+HighPropriety_Businesses = HPBusinessApplications(
+    r"z_src\fred_resources\fred_yamls\\HPBusinessApplications.yml"
+)
+House_Price_Index = HousePriceIdx(r"z_src\fred_resources\fred_yamls\\HousePriceIdx.yml")
+New_Housing = NewHousingPermits(
+    r"z_src\fred_resources\fred_yamls\\NewHousingPermits.yml"
+)
+Real_GDP = RealGDP(r"z_src\fred_resources\fred_yamls\\RealGDP.yml")
+HighPropriety_NAICs = TotalHighPropensityNAICs(
+    r"z_src\fred_resources\fred_yamls\\TotalHighPropensityNAICs.yml"
+)
+NAICs = TotalNAICs(r"z_src\fred_resources\fred_yamls\\TotalNAICs.yml")
+Unemployment_Rate = Unemployment(r"z_src\fred_resources\fred_yamls\\unemployment.yml")
+Zillow_Home_Value = ZillowHomeValue(
+    r"z_src\fred_resources\fred_yamls\\ZillowHomeValue.yml"
+)
+
+# load data
+uninterMaster = read_unintermaster()
+
+# INPUTS ----------------------------------------------------------------------
+cls = st.selectbox("Variable to Pull", options=class_list)
+
+# PULL DATA -------------------------------------------------------------------
+
+pulled_data = pull_all(cls)
+col_master = uninterMaster[pulled_data.columns]
+col_final = (
+    pd.concat([col_master, pulled_data])
+    .drop_duplicates()
+    .reset_index()
+    .drop(columns=["index"])
+    .groupby(["State", "Year", "Quarter", "Month"])
+    .agg("last")
+    .dropna()
+    .reset_index()
+)
+
+# DOWNLOADS -------------------------------------------------------------------
+st.header("Data")
+st.dataframe(col_final, use_container_width=True)
+
+
+@st.cache
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode("utf-8")
+
+
+col_csv = convert_df(col_final)
+st.download_button(
+    label="Download CSV",
+    data=col_csv,
+    file_name=f"{cls}.csv",
+    mime="text/csv",
+)
+
+# GRAPHS ----------------------------------------------------------------------
+st.header("Graphs")
+
+col1, col2 = st.columns(2)
+with col1:
+    selected_states = st.multiselect(
+        "Select Years to Graph",
+        col_final["State"].unique(),
+        default=["AK", "FL", "NM", "TX", "WA"],
+    )
+
+with col2:
+    trendline = st.radio(
+        "Trendline Options",
+        (None, "ols", "lowess", "expanding"),
+        horizontal=True,
+        help=("""Info [here](https://plotly.com/python/linear-fits/)"""),
+    )
+    scope = st.radio("Trendline Scope", (None, "state", "overall"), horizontal=True)
+
+nat_graphs_df = col_final[col_final["State"].isin(selected_states)]
+nat_graphs_df["Date"] = pd.to_datetime(
+    dict(year=nat_graphs_df.Year, month=nat_graphs_df.Month, day="01")
+)
+
+col = [
+    x
+    for x in nat_graphs_df.columns
+    if x not in ["State", "Year", "Quarter", "Month", "Date"]
+]
+
+
+if not scope:
+
+    fig = px.scatter(
+        nat_graphs_df,
+        x="Date",
+        y=col[0],
+        color="State",
+    )
+elif scope == "state":
+    fig = px.scatter(
+        nat_graphs_df,
+        x="Date",
+        y=col[0],
+        color="State",
+        trendline=trendline,
+    )
+else:
+    fig = px.scatter(
+        nat_graphs_df,
+        x="Date",
+        y=col[0],
+        color="State",
+        trendline=trendline,
+        trendline_scope=scope,
+        trendline_color_override="black",
+    )
+
+st.plotly_chart(fig, use_container_width=True)
+
+# CODES -----------------------------------------------------------------------
+st.subheader("Sources Pulled")
+
+
+@st.cache
+def master_codes():
+    df = pd.read_csv(r"z_src\final_datasets\master_codes.csv")
+    # df = df.drop(["Unnamed: 0"], axis=1)
+    df = df.groupby("Name")[
+        "frequency", "units", "seasonal_adjustment", "title", "id"
+    ].agg(pd.Series.mode)
+    return df
+
+
+codes_df = master_codes()
+st.dataframe(codes_df)
+st.caption("*All data taken from [*FRED*](https://fred.stlouisfed.org/)")
---
+++ pages/3_🤖_machine_explainer.py
@@ -0,0 +1,81 @@
+# IMPORTS ---------------------------------------------------------------------
+import pandas as pd
+
+# streamlit
+import streamlit as st
+
+
+###############################################################################
+# SET UP ----------------------------------------------------------------------
+st.set_page_config(page_icon="🤖", page_title="Machine Explainer", layout="wide")
+st.image(
+    "https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/robot_1f916.png",
+    width=100,
+)
+st.title("Machine Learning Model Explanation")
+
+
+# IMPORTANCE ------------------------------------------------------------------
+st.header("Importance")
+
+
+@st.cache
+def pull_importance():
+    df = pd.read_csv(r"z_src\models\tables\important_table.csv")
+    df = df.drop(columns=["Unnamed: 0"])
+    return df
+
+
+importance_df = pull_importance()
+st.dataframe(importance_df)
+
+st.markdown(
+    """
+## What is XGBoost?
+XGBoost stands for Extreme Gradient Boosting. It relies on the intuition that the best possible 
+next model, when combined with previous models, minimizes the overall prediction error.
+
+## What is Gradient Boosting?
+Gradient Boosting: It is an additive and sequential model where trees are grown in sequential
+manner which converts weak learners into strong learners by adding weights to the weak learners 
+and reduce weights of the strong learners. So each tree learns and boosts from the previous tree grown.
+
+## Why Choose XGBoost?
+This type of Machine learning is best for our data set. We have a rectangular small data set 
+that an XGBoost model can easily move through and train itself on. We looked at doing a neural
+network but after multiple attempts we concluded that we would need much more data in order to 
+justify the use of a Neural Network.
+
+## How is the model trained?
+We created multiple models to predict 6 months, 12 months, 18 months, 24 months, 30 months, and 36
+months into the future. For our model, we looked at the following variables: unemployment, construction wages, 
+number of construction employees, business applications, high propensity business applications, GDP, 
+house price index, new housing permits, and Zillow home values. 
+
+We took all this data and filtered for just the national values. We then split the national data 
+into two different datasets, Train and Test. We threw theTrain dataset in our model to create a model.
+From this model, we took the Test dataset and ran our model through it. By doing so we can measure
+how well our model is performing. The Metrics data table calculatesr-squared values and mean squared error values.
+
+    """
+)
+
+# METRICS ---------------------------------------------------------------------
+st.header("Metrics")
+
+
+@st.cache
+def pull_metrics():
+    df = pd.read_csv(r"z_src\models\tables\metrics_table.csv")
+    df = df.drop(columns=["Unnamed: 0"])
+    return df
+
+
+metrics_df = pull_metrics()
+st.dataframe(metrics_df)
+
+st.markdown(
+    """
+    You can write in markdown here
+    """
+)
